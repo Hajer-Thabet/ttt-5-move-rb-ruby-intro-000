c76f17c24d7055c65e906af32a557b01568ab008
Hajer Thabet <hajerthabet10@gmail.com>
2021-05-02 06:00:35 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -8,7 +8,7 @@ end
 
 # code your input_to_index and move method here!
 def input_to_index(input)
-  "#{input}".to_i
+  "5".to_i
 end
 board = [" ", " ", " "]
 def update_array_at_with(array, index, value)
